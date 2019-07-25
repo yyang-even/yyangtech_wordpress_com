ab1d881f2fc1bc3d6f7cbd8e9af1c4cd3253a78c
Yang Yang <yyang.even@gmail.com>
2019-07-25 15:28:29 -0700

    2019_07_11: A CTest That Never Ends?
    Change-Id: Ica808d3b1d2ba536b290117c48b41eefab59265a

---
+++ 2019_07_11/setup_db_environment.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+import subprocess
+
+
+DBFARM_PATH="/tmp/dbfarm"
+subprocess.call(["monetdbd", "start", DBFARM_PATH], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
