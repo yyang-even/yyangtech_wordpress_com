43f68a9a66dc036e7dcc5c1d987fe06031725f8b
Yang Yang <yyang.even@gmail.com>
2020-09-09 13:35:39 -0700

    Pre-commit: Enable pre-commit
    Change-Id: I6ad15f0c9154cf7303a7753658f0bebda0ebb3f2

--- 2019_07_11/setup_db_environment.py
+++ 2019_07_11/setup_db_environment.py
@@ -3,5 +3,10 @@
 import subprocess
 
 
-DBFARM_PATH="/tmp/dbfarm"
-subprocess.call(["monetdbd", "start", DBFARM_PATH], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+DBFARM_PATH = "/tmp/dbfarm"
+subprocess.call(
+    ["monetdbd", "start", DBFARM_PATH],
+    stdin=subprocess.PIPE,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![pre-commit](https://img.shields.io/badge/pre--commit-enabled-brightgreen?logo=pre-commit&logoColor=white)](https://github.com/pre-commit/pre-commit)
+
 # yyangtech_wordpress_com
 Sample code for my blog [yyangtech.wordpress.com](https://yyangtech.wordpress.com/).
